f9c7df678c34b3d5dda635554c71037435ff0b8d
Ivan Popov <ivan.alekseevich.popov@gmail.com>
2023-01-08 18:38:58 +0400

    added test cron command for platform

---
+++ src/Command/PartsFindCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:parts:find',
+    description: 'Command to search parts and their images in the internet',
+)]
+class PartsFindCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        for ($i=0; $i < 100; $i++) {
+            sleep(10);
+            $output->writeln("$i. Still working...");
+        }
+        return Command::SUCCESS;
+    }
+}
