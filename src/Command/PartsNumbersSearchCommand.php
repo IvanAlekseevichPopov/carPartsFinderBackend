824e35fd4db4ede833d655f690c061adc5a6ae9b
Ivan Popov <ivan.alekseevich.popov@gmail.com>
2023-01-28 22:03:30 +0400

    moved client in separate factory

--- src/Command/PartsNumbersSearchCommand.php
+++ src/Command/PartsNumbersSearchCommand.php
@@ -4,10 +4,17 @@ namespace App\Command;
 
 use App\Entity\Brand;
 use App\Entity\CarModel;
+use App\Entity\Part;
+use App\Entity\PartName;
 use App\Repository\BrandRepository;
+use App\Repository\CarModelRepository;
+use App\Repository\PartNameRepository;
+use App\Repository\PartRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\ServerException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -23,39 +30,33 @@ class PartsNumbersSearchCommand extends Command
 {
     private EntityManagerInterface $entityManager;
     private BrandRepository $brandRepository;
+    private CarModelRepository $carModelRepository;
+    private PartNameRepository $partNameRepository;
+    private PartRepository $partRepository;
+    private FilesystemAdapter $cache;
+
     private SymfonyStyle $io;
+    private Client $client;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        BrandRepository        $manufacturerRepository,
-        string                 $name = null
-    )
-    {
+        BrandRepository $manufacturerRepository,
+        CarModelRepository $carModelRepository,
+        PartNameRepository $partNameRepository,
+        PartRepository $partRepository,
+        Client $client,
+        FilesystemAdapter $cache,
+        string $name = null
+    ) {
         parent::__construct($name);
 
         $this->entityManager = $entityManager;
         $this->brandRepository = $manufacturerRepository;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRandomUserAgent(): string
-    {
-        switch (random_int(0, 4)) {
-            case 0:
-                return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
-            case 1:
-                return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0';
-            case 2:
-                return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15';
-            case 3:
-                return 'Opera/9.96 (X11; Linux x86_64; sl-SI) Presto/2.12.343 Version/11.00';
-            case 4:
-                return 'Mozilla/5.0 (Windows NT 6.1; sl-SI; rv:1.9.2.20) Gecko/20171125 Firefox/37.0';
-        }
-        return 'Mozilla/5.0 (Windows 98) AppleWebKit/5311 (KHTML, like Gecko) Chrome/37.0.840.0 Mobile Safari/5311';
-
+        $this->carModelRepository = $carModelRepository;
+        $this->partNameRepository = $partNameRepository;
+        $this->partRepository = $partRepository;
+        $this->cache = $cache;
+        $this->client = $client;
     }
 
     protected function configure(): void
@@ -68,8 +69,11 @@ class PartsNumbersSearchCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->io = new SymfonyStyle($input, $output);
-        $this->parseBrands();
-        $this->parseCarModels();
+
+//        $this->parseBrands();
+//        $this->parseCarModels();
+
+        $this->parsePartsNumbers();
 
         return Command::SUCCESS;
     }
@@ -102,51 +106,144 @@ class PartsNumbersSearchCommand extends Command
     private function parseCarModels()
     {
         $brands = $this->brandRepository->findAllToParseModels();
-        $client = new Client();
 
         $this->io->writeln('Start parsing car models');
         $this->io->progressStart(count($brands));
         foreach ($brands as $brand) {
-            try {
-                $res = $client->request(
-                    'GET',
-                    "https://tecdoc.autodoc.ru/api/catalogs/tecdoc/brands/{$brand->getExternalId()}/models",
-                    [
-                        'headers' => [
-                            'User-Agent' => $this->getRandomUserAgent(),
-                            'Accept-Language' => 'en,ru;q=0.9',
-                            'referer' => 'https://www.autodoc.ru/',
-//                            'sec-ch-ua' => '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-                        ]
-                    ]);
-                $modelsResponse = json_decode($res->getBody()->getContents(), true);
+            $res = $this->client->get("/api/catalogs/tecdoc/brands/{$brand->getExternalId()}/models");
+            $modelsResponse = json_decode($res->getBody()->getContents(), true);
 
-                foreach ($modelsResponse['models'] as $rawModelData) {
-                    $carModel = new CarModel($brand, $rawModelData['name'], $rawModelData['id']);
-                    if(!empty($rawModelData['yearFrom'])) {
-                        $carModel->setProductionStart(\DateTimeImmutable::createFromFormat('Ymd', $rawModelData['yearFrom'] . '01'));
-                    }
-                    if (!empty($rawModelData['yearTo'])) {
-                        $carModel->setProductionFinish(\DateTimeImmutable::createFromFormat('Ymd', $rawModelData['yearTo'] . '01'));
-                    }
-                    $this->entityManager->persist($carModel);
+            foreach ($modelsResponse['models'] as $rawModelData) {
+                $carModel = new CarModel($brand, $rawModelData['name'], $rawModelData['id']);
+                if (!empty($rawModelData['yearFrom'])) {
+                    $carModel->setProductionStart(\DateTimeImmutable::createFromFormat('Ymd', $rawModelData['yearFrom'].'01'));
                 }
-
-                $brand->setChildrenModelsParsed(true);
-                $this->entityManager->flush();
-                $this->io->progressAdvance();
-                $this->io->writeln(" | {$brand->getName()} filled: " .count($modelsResponse['models']));
-
-                $time = random_int(1, 4);
-                $this->io->writeln("Waiting {$time} seconds");
-                sleep($time);
-            } catch (\Throwable $exception) {
-                dump($modelsResponse, $rawModelData) ;
-                throw $exception;
+                if (!empty($rawModelData['yearTo'])) {
+                    $carModel->setProductionFinish(\DateTimeImmutable::createFromFormat('Ymd', $rawModelData['yearTo'].'01'));
+                }
+                $this->entityManager->persist($carModel);
             }
+
+            $brand->setChildrenModelsParsed(true);
+            $this->entityManager->flush();
+            $this->io->progressAdvance();
+            $this->io->writeln(" | {$brand->getName()} filled: ".count($modelsResponse['models']));
+
+            $time = random_int(1, 4);
+            $this->io->writeln("Waiting {$time} seconds");
+            sleep($time);
         }
 
         $this->io->progressFinish();
+    }
+
+    private function parsePartsNumbers()
+    {
+        $models = $this->carModelRepository->findAllToParsePartsNumbers();
+        $this->io->writeln('Start parsing car models');
+        $this->io->progressStart(count($models));
+        foreach ($models as $model) {
+            $brand = $model->getBrand();
+            $res = $this->client->get("/api/catalogs/tecdoc/brands/{$brand->getExternalId()}/models/{$model->getExternalId()}/modifications");
+            $modificationsResponse = json_decode($res->getBody()->getContents(), true);
+
+            foreach ($modificationsResponse['modifications'] as $rawModificationData) {
+                $alreadyProcessed = $this->cache->getItem("modification_{$rawModificationData['id']}");
+                if ($alreadyProcessed->isHit()) {
+                    dump("Modification {$rawModificationData['id']} already processed");
+                    continue;
+                }
+                $nodesRes = $this->client->get("/api/catalogs/tecdoc/brands/{$brand->getExternalId()}/models/{$model->getExternalId()}/modifications/{$rawModificationData['id']}/nodes");
+
+                $nodesResponse = json_decode($nodesRes->getBody()->getContents(), true);
+                foreach ($nodesResponse['nodes'] as $rawNodeData) {
+                    $this->processNode($rawNodeData, $rawModificationData['id'], $model);
+                }
+                $alreadyProcessed->set(true);
+                $this->cache->save($alreadyProcessed);
+            }
+            $model->setModifications($modificationsResponse['modifications']);
+            $model->setChildrenPartsParsed(true);
+            $this->entityManager->flush();
+            $this->entityManager->clear();
+        }
+    }
+
+    private function processNode(array $rawNodeData, int $modificationId, CarModel $model)
+    {
+        foreach ($rawNodeData['children'] as $rawNodeData) {
+            $this->processNode($rawNodeData, $modificationId, $model);
+        }
+
+        $id = $rawNodeData['id'];
+        $alreadyProcessed = $this->cache->getItem("node_{$modificationId}_{$id}");
+        if ($alreadyProcessed->isHit()) {
+            dump("Node {$modificationId}_{$id} already processed");
+
+            return;
+        }
+
+        $brand = $model->getBrand();
+        try {
+            $sparePartsRes = $this->client->get("/api/catalogs/tecdoc/brands/{$brand->getExternalId()}/models/{$model->getExternalId()}/modifications/{$modificationId}/nodes/{$id}/spareparts");
+
+            $sparePartsResponse = json_decode($sparePartsRes->getBody()->getContents(), true);
+
+            foreach ($sparePartsResponse['spareParts'] as $rawSparePartData) {
+                $brand = $this->findOrCreateBrand($rawSparePartData['brandName'], $rawSparePartData['autodocBrandId']);
+                $partName = $this->findOrCreatePartName($rawSparePartData['name']);
+
+                $this->findOrCreatePart($brand, $partName, $model, $rawSparePartData['partNumber'], $rawSparePartData['imageUrls'] ?? null);
+                $this->entityManager->flush();
+            }
+        } catch (ServerException $e) {
+            dump($e->getMessage());
+
+            $item = $this->cache->getItem('failed_nodes_'.$brand->getName());
+            $value = $item->get() ?? [];
+            $value[] = $modificationId.'|'.$id;
+            $item->set($value);
+        }
+
+        $alreadyProcessed->set(true);
+        $this->cache->save($alreadyProcessed);
+    }
+
+    private function findOrCreateBrand(string $brandName, int $autodocBrandId): Brand
+    {
+        $brand = $this->brandRepository->findOneBy(['externalId' => $autodocBrandId]);
+        if (!$brand) {
+            $brand = new Brand($brandName, $autodocBrandId);
+            $this->entityManager->persist($brand);
+        }
+
+        return $brand;
+    }
+
+    private function findOrCreatePartName(string $name): PartName
+    {
+        $partName = $this->partNameRepository->findOneBy(['name' => $name]);
+        if (!$partName) {
+            $partName = new PartName($name);
+            $this->entityManager->persist($partName);
+        }
+
+        return $partName;
+    }
+
+    private function findOrCreatePart(Brand $brand, PartName $partName, CarModel $model, string $partNumber, ?array $images = null): Part
+    {
+        $part = $this->partRepository->findOneBy(['partNumber' => $partNumber]);
+        if (!$part) {
+            $part = new Part($partNumber, $partName, $brand);
+            if (!empty($images)) {
+                $part->setImagesToParse($images);
+            }
+            $this->entityManager->persist($part);
+        }
+        $part->addSuitableModel($model);
+
+        return $part;
     }
 }
 
---
+++ src/Service/Factory/ParserClientFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Factory;
+
+use GuzzleHttp\Client;
+
+class ParserClientFactory
+{
+    private const TIMEOUT = 10;
+
+    public static function create(string $baseUrl, string $referer): Client
+    {
+        return new Client([
+            'base_uri' => $baseUrl,
+            'timeout' => self::TIMEOUT,
+            'headers' => self::selectRandomHeadersCollection($referer),
+        ]);
+    }
+
+    private static function selectRandomHeadersCollection(string $referer): array
+    {
+        switch (random_int(1, 2)) {
+            case 1:
+                return [
+                    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+                    'Accept' => 'application/json, text/plain, */*',
+                    'Accept-Language' => 'en,ru;q=0.9',
+                    'Accept-Encoding' => 'gzip, deflate, br',
+                    'Connection' => 'keep-alive',
+                    'Origin' => $referer,
+                    'Referer' => $referer.'/',
+                    'Sec-ch-ua' => '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+                    'Sec-ch-ua-mobile' => '?0',
+                    'Sec-fetch-dest' => 'empty',
+                    'Sec-fetch-mode' => 'cors',
+                    'Sec-fetch-site' => 'same-site',
+                ];
+            case 2:
+                return [
+                    'User-Agent' => 'Opera/8.24 (X11; Linux i686; en-US) Presto/2.11.273 Version/10.00',
+                    'Accept' => 'application/json, text/plain, */*',
+                    'Accept-Language' => 'en,ru;q=0.9',
+                    'Accept-Encoding' => 'gzip, deflate, br',
+                    'Connection' => 'keep-alive',
+                    'Origin' => $referer,
+                    'Referer' => $referer.'/',
+                    'Sec-ch-ua' => 'Sec-CH-UA: "Opera";v="10", " Not;A Brand";v="69", "Chromium";v="5"',
+                    'Sec-ch-ua-mobile' => '?0',
+                    'Sec-fetch-dest' => 'empty',
+                    'Sec-fetch-mode' => 'cors',
+                    'Sec-fetch-site' => 'same-site',
+                ];
+        }
+        throw new \LogicException('No headers collection selected');
+    }
+}
